a46833265d99cd9b1ae346002e28801f2c771684
PCO_922 <oussenecamara717@gmail.com>
2024-03-27 12:46:39 +0100

    test du compareto

--- Datetest.java
+++ Datetest.java
@@ -25,5 +25,21 @@ class DateTest {
     }
 
 
+    @Test
+    void compareTo() {
+        Date date1 = new Date(2024, 3, 25);
+        Date date2 = new Date(2024, 3, 26);
+        Date date3 = new Date(2024, 3, 25);
+        
+        // Test for comparison when date1 is before date2
+        assertTrue(date1.compareTo(date2) < 0);
+        
+        // Test for comparison when date2 is after date1
+        assertTrue(date2.compareTo(date1) > 0);
+        
+        // Test for comparison when date1 is equal to date3
+        assertEquals(0, date1.compareTo(date3));
+    }
+
     
 }
